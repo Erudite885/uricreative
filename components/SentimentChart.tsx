129457a8cbcc89d31fa18b8d9edec0be6382af77
Erudite885 <erudite885@gmail.com>
2025-04-06 15:23:05 +0100

    improved sentiment chart

--- components/SentimentChart.tsx
+++ components/SentimentChart.tsx
@@ -1,19 +1,65 @@
+// components/SentimentChart.tsx
 import { LineChart } from "@mui/x-charts/LineChart";
-import { memo } from "react";
+import { Box } from "@mui/material";
+import { useMemo } from "react";
 
-type Props = {
-  data: { date: string; sentiment: number }[];
-};
+interface TrendPoint {
+  date: string;
+  sentiment: number;
+}
 
-const SentimentChart = ({ data }: Props) => {
+interface SentimentChartProps {
+  data: TrendPoint[];
+}
+
+export default function SentimentChart({ data }: SentimentChartProps) {
+  // Memoized values for performance
+  const { dates, sentiments, minIndex, maxIndex } = useMemo(() => {
+    const sentiments = data.map((d) => d.sentiment);
+    const dates = data.map((d) => d.date);
+
+    const minSentiment = Math.min(...sentiments);
+    const maxSentiment = Math.max(...sentiments);
+
+    const minIndex = sentiments.indexOf(minSentiment);
+    const maxIndex = sentiments.indexOf(maxSentiment);
+
+    return { dates, sentiments, minIndex, maxIndex };
+  }, [data]);
+
   return (
-    <LineChart
-      xAxis={[{ data: data.map((d) => d.date), scaleType: "point" }]}
-      series={[{ data: data.map((d) => d.sentiment) }]}
-      height={300}
-      sx={{ maxWidth: "100%" }}
-    />
+    <Box
+      sx={{
+        width: "100%",
+        maxWidth: "100%",
+        height: { xs: 300, sm: 400 },
+        overflowX: "auto", // Just in case x-axis labels overflow
+      }}
+    >
+      <LineChart
+        xAxis={[{ scaleType: "point", data: dates }]}
+        series={[
+          {
+            data: sentiments,
+            color: "#1976d2",
+            showMark: true,
+            highlightScope: {
+              highlighted: "item",
+              faded: "global",
+            },
+            highlightedMarkStyle: ({ index }) => {
+              if (index === minIndex) return { fill: "red", r: 6 };
+              if (index === maxIndex) return { fill: "green", r: 6 };
+              return { r: 3 };
+            },
+          },
+        ]}
+        height={300}
+        interaction={{
+          zoom: true,
+          pan: true,
+        }}
+      />
+    </Box>
   );
-};
-
-export default memo(SentimentChart);
+}

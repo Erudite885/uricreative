b01885d0e90aaed47db524e380a67277a498daae
Erudite885 <erudite885@gmail.com>
2025-04-06 11:31:55 +0100

    dropdown to switch between hash tags

--- src/pages/insights/[hashtag].tsx
+++ src/pages/insights/[hashtag].tsx
@@ -1,14 +1,33 @@
 // pages/insights/[hashtag].tsx
-import { useRouter } from "next/router";
+import { useRouter, usePathname } from "next/navigation";
 import HashtagTrendCard from "../../components/HashtagTrendCard";
+import { Box, Button, Select, MenuItem } from "@mui/material";
 
 export default function HashtagInsightsPage() {
   const router = useRouter();
+  const pathname = usePathname();
   const { hashtag } = router.query;
 
-  if (!hashtag) {
-    return <div>Loading...</div>;
-  }
+  const handleHashtagChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedHashtag = event.target.value;
+    router.push(`/insights/${selectedHashtag}`);
+  };
 
-  return <HashtagTrendCard hashtag={hashtag as string} />;
+  return (
+    <Box sx={{ p: 2 }}>
+      <Box sx={{ mb: 2 }}>
+        <Select
+          value={hashtag || ""}
+          onChange={handleHashtagChange}
+          label="Select Hashtag"
+          fullWidth
+        >
+          <MenuItem value="uri">#uri</MenuItem>
+          <MenuItem value="example">#example</MenuItem>
+          <MenuItem value="test">#test</MenuItem>
+        </Select>
+      </Box>
+      <HashtagTrendCard hashtag={hashtag as string} />
+    </Box>
+  );
 }

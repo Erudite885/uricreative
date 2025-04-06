66993fe41fe35c22273b9d1ca513587bd1269e5c
Bianca Uche <79880772+Erudite885@users.noreply.github.com>
2025-04-06 19:14:03 +0100

    Update index.tsx update

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,9 +14,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
-            Get started{" "}
-            
-            .
+            Get started🚀
           </li>
           
         </ol>
@@ -24,7 +22,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://www.uricreative.vercel.app/insights/uri"
+            href="https://uricreative.vercel.app/insights/uri"
             target="_blank"
             rel="noopener noreferrer"
           >

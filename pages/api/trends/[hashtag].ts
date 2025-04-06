606bc031d6c40e31c0d64383ae5783a90d9d736a
Erudite885 <erudite885@gmail.com>
2025-04-06 15:23:42 +0100

    map method adjusted

--- pages/api/trends/[hashtag].ts
+++ pages/api/trends/[hashtag].ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { trendData } from "../../../mocks/trendData";
+import { trendDataMap } from "../../../mocks/trendData";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { hashtag } = req.query;
@@ -10,9 +10,10 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     return res.status(400).json({ error: "Invalid hashtag" });
   }
 
-  if (hashtag.toLowerCase() === "uri") {
-    return res.status(200).json(trendData);
+  const data = trendDataMap[hashtag.toLowerCase()];
+  if (!data) {
+    return res.status(404).json({ error: "Hashtag not found" });
   }
 
-  return res.status(404).json({ error: "Hashtag not found" });
+  return res.status(200).json(data);
 }
